475cc7b139a6420c2b2c4cbcd61a72826fc3d4cd
Tangatar <sangishievt@gmail.com>
2025-05-14 01:25:29 +0300

    Edit name site

--- src/widgets/Footer.tsx
+++ src/widgets/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="w-full flex py-4 px-[20px] bg-p bg-purple-900">
             <span className="uppercase font-black text-2xl">
-                Force
+                Gdegang
             </span>
         </footer>
     )
--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header className="w-full flex py-4 px-[20px] bg-p bg-purple-950">
             <span className="uppercase font-black text-2xl">
-                Force
+                Gdegang
             </span>
         </header>
     )
